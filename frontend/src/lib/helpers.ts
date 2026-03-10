eac0b05d25d9b07cec2dc8c20691e425ba41376d
sunil8521 <heyitsmevro@gmail.com>
2026-03-10 23:55:44 +0530

    chnage code base

--- frontend/src/lib/helpers.ts
+++
@@ -1,8 +0,0 @@
-function generateCode(): string {
-  const randomPart = () =>
-    Array.from({ length: 3 }, () =>
-      String.fromCharCode(97 + Math.floor(Math.random() * 26))
-    ).join("");
-  return `${randomPart()}-${randomPart()}-${randomPart()}`;
-}
-export { generateCode };
--- frontend/src/lib/socket.ts
+++
@@ -1,5 +0,0 @@
-import { io, Socket } from "socket.io-client";
-
-const URL = "http://localhost:3000";
-export const socket: Socket = io(URL, {});
-
