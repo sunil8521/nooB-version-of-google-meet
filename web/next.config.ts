445ad0a6bb72422c06a11a18f8cc22333e0c86cb
sunil8521 <heyitsmevro@gmail.com>
2026-07-06 02:08:16 +0530

    Fix all TS type errors properly using npmrc shamefully-hoist

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,13 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // Ignored because of internal Next.js 16 ResolvingMetadata error
-    ignoreBuildErrors: true,
-  },
 };
 
 export default nextConfig;
